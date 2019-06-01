b7a8e5c78d9006611ca93abe1d8c8d0fc123f791
qsong <qsong.vip@qq.com>
2019-06-01 18:15:33 +0800

    commit structure of jinshuju

--- junyi/client/classinfo.py
+++ junyi/client/classinfo.py
@@ -3,18 +3,31 @@
 from junyi.server.server import Server
 from junyi.utils import log
 
+
 class ClassInfoWJ(Server):
 
     # 类变量：发放的总问卷数量
-    total_counter = 0
+    __total_counter = 0
     # 类变量：回收的有效问卷数量
-    valid_counter = 0
+    __valid_counter = 0
     # 类变量，所有存储的数据
-    total_answers_list = []
+    __total_answers_list = []
+
+    @classmethod
+    def get_total_counter(cls):
+        return cls.__total_counter
+
+    @classmethod
+    def get_valid_counter(cls):
+        return cls.__valid_counter
+
+    @classmethod
+    def get_total_answers_list(cls):
+        return cls.__total_answers_list
 
     def __init__(self):
         log.debug("我是ClassInfoWJ的构造方法，只要发一份问卷，我就会执行一次")
-        ClassInfoWJ.total_counter += 1
+        ClassInfoWJ.__total_counter += 1
 
     def answer(self, name, tel, email):
         '''
@@ -29,10 +42,8 @@ class ClassInfoWJ(Server):
             'tel': self.tel,
             'email': self.email
         }
-        ClassInfoWJ.valid_counter += 1
-        ClassInfoWJ.total_answers_list.append(self.current_wj_anwser)
-
-
+        ClassInfoWJ.__valid_counter += 1
+        ClassInfoWJ.__total_answers_list.append(self.current_wj_anwser)
 
     def get_answer(self):
         '''
--- junyi/test/test_classinfowj.py
+++ junyi/test/test_classinfowj.py
@@ -6,26 +6,33 @@ from junyi.client.classinfo import ClassInfoWJ
 class TestClassInfoWJ(unittest.TestCase):
 
     def test_get_current_wj_counter(self):
-        print("当前已发放问卷数量: %s" % ClassInfoWJ.total_counter)
-        print("收回有效问卷数量: %s" % ClassInfoWJ.valid_counter)
-        print("收回有效问卷答案列表: %s" % ClassInfoWJ.total_answers_list)
+        # 查看问卷后台，获取当前提交信息
+        print("当前已发放问卷数量: %s" % ClassInfoWJ.get_total_counter())
+        print("收回有效问卷数量: %s" % ClassInfoWJ.get_valid_counter())
+        print("收回有效问卷答案列表: %s" % ClassInfoWJ.get_total_answers_list())
 
+        # glc 扫码打开了一份问卷，并提交了信息
         glc_wj = ClassInfoWJ()
         glc_wj.answer('glc', '19987654321', 'wrhew@qq.com')
-        print("当前已发放问卷数量: %s" % ClassInfoWJ.total_counter)
-        print("收回有效问卷数量: %s" % ClassInfoWJ.valid_counter)
-        print("收回有效问卷答案列表: %s" % ClassInfoWJ.total_answers_list)
+        # 查看问卷后台，获取当前提交信息
+        print("当前已发放问卷数量: %s" % ClassInfoWJ.get_total_counter())
+        print("收回有效问卷数量: %s" % ClassInfoWJ.get_valid_counter())
+        print("收回有效问卷答案列表: %s" % ClassInfoWJ.get_total_answers_list())
 
+        # hx 扫码打开了一份问卷，没有提交信息
         hx_wj = ClassInfoWJ()
-        print("当前已发放问卷数量: %s" % ClassInfoWJ.total_counter)
-        print("收回有效问卷数量: %s" % ClassInfoWJ.valid_counter)
-        print("收回有效问卷答案列表: %s" % ClassInfoWJ.total_answers_list)
+        # 查看问卷后台，获取当前提交信息
+        print("当前已发放问卷数量: %s" % ClassInfoWJ.get_total_counter())
+        print("收回有效问卷数量: %s" % ClassInfoWJ.get_valid_counter())
+        print("收回有效问卷答案列表: %s" % ClassInfoWJ.get_total_answers_list())
 
+        # kjp 扫码打开了一份问卷，并提交了信息
         kjp_wj = ClassInfoWJ()
+        # 查看问卷后台，获取当前提交信息
         kjp_wj.answer('kjp', '19987687990', 'kjp@qq.com')
-        print("当前已发放问卷数量: %s" % ClassInfoWJ.total_counter)
-        print("收回有效问卷数量: %s" % ClassInfoWJ.valid_counter)
-        print("收回有效问卷答案列表: %s" % ClassInfoWJ.total_answers_list)
+        print("当前已发放问卷数量: %s" % ClassInfoWJ.get_total_counter())
+        print("收回有效问卷数量: %s" % ClassInfoWJ.get_valid_counter())
+        print("收回有效问卷答案列表: %s" % ClassInfoWJ.get_total_answers_list())
 
     def test_customer_answer(self):
         #1. 获取了问卷的一个实例：（扫描，在页面打开的一个问卷）
